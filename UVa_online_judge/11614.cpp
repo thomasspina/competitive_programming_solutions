84bc05c961641bd229232f2ab6872f8e2190f4c6
thomasspina <thomas@thomasspina.com>
2026-01-12 17:55:33 -0500

    add solution 11614

---
+++ UVa_online_judge/11614.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+
+int main() {
+    int t; scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        ull n; scanf("%llu", &n);
+
+        // 1 + 2 + 3 + ... + k = [k * (k + 1)] / 2 = n. solve for k
+        
+        double r = (-1 + sqrt(1 + 8 * n)) / 2;
+        
+        printf("%d\n", (int) r);
+    }
+
+    return 0;
+}
